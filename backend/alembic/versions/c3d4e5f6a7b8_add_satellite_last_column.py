1041fe19282a076edeb4e017bb2b086182f0a4db
DIVAGAR E <divagareofficial10@gmail.com>
2026-08-26 09:48:59 +0530

    fix: resolve migration head chain for satellite_last

--- backend/alembic/versions/c3d4e5f6a7b8_add_satellite_last_column.py
+++ backend/alembic/versions/c3d4e5f6a7b8_add_satellite_last_column.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'c3d4e5f6a7b8'
-down_revision: Union[str, Sequence[str], None] = 'f8a9b2c3d4e5'
+down_revision: Union[str, Sequence[str], None] = 'b2c3d4e5f6a7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
